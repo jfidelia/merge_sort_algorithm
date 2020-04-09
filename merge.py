a69ca4aa996f649503700d8d7705d74409af4181
Jacques Fidelia Jr <jacques.fidelia@gmail.com>
2020-04-09 22:25:01 +0000

    n-ary tree commit

--- merge.py
+++ merge.py
@@ -1,38 +1,42 @@
-def msort(mylist, left, right):
-    if right - left > 1:
-        middle = (left + right)//2
-        msort(mylist, left, middle)
-        msort(mylist, middle, right)
-        mlist(mylist, left, middle, right)
+# def msort(mylist, left, right):
+#     if right - left > 1:
+#         middle = (left + right)//2
+#         msort(mylist, left, middle)
+#         msort(mylist, middle, right)
+#         mlist(mylist, left, middle, right)
  
-def mlist(mylist, left, middle, right):
-    leftlist = mylist[left:middle]
-    rightlist = mylist[middle:right]
-    k = left
-    i = 0
-    j = 0
-    while (left + i < middle and middle + j < right):
-        if (leftlist[i] <= rightlist[j]):
-            mylist[k] = leftlist[i]
-            i = i + 1
-        else:
-            mylist[k] = rightlist[j]
-            j = j + 1
-        k = k + 1
-    if left + i < middle:
-        while k < right:
-            mylist[k] = leftlist[i]
-            i = i + 1
-            k = k + 1
-    else:
-        while k < right:
-            mylist[k] = rightlist[j]
-            j = j + 1
-            k = k + 1
+# def mlist(mylist, left, middle, right):
+#     leftlist = mylist[left:middle]
+#     rightlist = mylist[middle:right]
+#     k = left
+#     i = 0
+#     j = 0
+#     while (left + i < middle and middle + j < right):
+#         if (leftlist[i] <= rightlist[j]):
+#             mylist[k] = leftlist[i]
+#             i = i + 1
+#         else:
+#             mylist[k] = rightlist[j]
+#             j = j + 1
+#         k = k + 1
+#     if left + i < middle:
+#         while k < right:
+#             mylist[k] = leftlist[i]
+#             i = i + 1
+#             k = k + 1
+#     else:
+#         while k < right:
+#             mylist[k] = rightlist[j]
+#             j = j + 1
+#             k = k + 1
   
   
-mylist = input('Enter the list values to be sorted: ').split()
-mylist = (int(x) for x in mylist)
-msort(mylist, 0, len(mylist))
-print('The sorted list is: ')
-print(mylist)
+# mylist = input('Enter the list values to be sorted: ').split()
+# mylist = (int(x) for x in mylist)
+# msort(mylist, 0, len(mylist))
+# print('The sorted list is: ')
+# print(mylist)
+
+def example1(manatees):
+    for manatee in manatees:
+        print (manatee[0]['name'])
---
+++ nAry_start.py
@@ -0,0 +1,61 @@
+from collections import defaultdict
+
+class TrieNode:
+    def __init__(self, char):
+        self.children = []
+        self.isWord = False
+        self.val = char
+
+class Trie(object):
+
+    def __init__(self):
+        self.root = TrieNode('*')        
+
+    def insert(self, word):
+        current = self.root
+        for w in word:
+            flag = False
+            for child in current.children:
+                if child.val == w:
+                    flag = True
+                    current = child
+                    break
+            if not flag:
+                new_node = TrieNode(w)
+                current.children.append(new_node)
+                current = new_node
+        current.isWord = True
+
+    def search(self, word):
+        current = self.root
+        for w in word:
+            found = False
+            for child in current.children:
+                if child.val == w:
+                    found = True
+                    current = child
+                    break
+            if not found:
+                return False
+        return current.isWord
+        
+
+    def startsWith(self, prefix):
+        current = self.root
+        for w in prefix:
+            found = False
+            for child in current.children:
+                if child.val == w:
+                    found = True
+                    current = child
+                    break
+            if not found:
+                return False
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(hello)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
