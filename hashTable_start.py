ccc070565e8bc36820e40031c1094ffe9e12b107
Jacques Fidelia Jr <jacques.fidelia@gmail.com>
2020-04-10 02:30:56 +0000

    hash table implementation commit

---
+++ hashTable_start.py
@@ -0,0 +1,10 @@
+# Key: State
+#Value: Capital
+
+statesToCapitals = {}
+
+statesToCapitals["Texas"] = "Austin"
+statesToCapitals["New York"] = "Albany"
+
+print(statesToCapitals["New York"])
+print(statesToCapitals["Texas"])
