c10877673594f6770560b476a3ebfe056eff6a5f
Jacques Fidelia Jr <jacques.fidelia@gmail.com>
2020-04-20 21:38:23 +0000

    day 4 commit

--- hashTable_start.py
+++ hashTable_start.py
@@ -13,3 +13,19 @@ print(statesToCapitals["New York"])
 print(statesToCapitals["Texas"])
 print(newStatesToCapitals["New Jersey"])
 print(newStatesToCapitals["California"])
+
+# Key: State
+#Value: Capital
+
+statesToCapitals = {}
+newStatesToCapitals = {}
+
+statesToCapitals["Texas"] = "Austin"
+statesToCapitals["New York"] = "Albany"
+newStatesToCapitals["New Jersey"] = "Trenton"
+newStatesToCapitals["California"] = "Sacramento"
+
+print(statesToCapitals["New York"])
+print(statesToCapitals["Texas"])
+print(newStatesToCapitals["New Jersey"])
+print(newStatesToCapitals["California"])
