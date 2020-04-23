b37058786b240aa35a5d2774baf4929de74cd5be
Jacques Fidelia Jr <jacques.fidelia@gmail.com>
2020-04-23 14:30:21 +0000

    additional commit

--- recursion.py
+++ recursion.py
@@ -11,3 +11,10 @@ def factorial(number):
     return number * factorial(number - 1)
 
 print(factorial(5))
+
+def factorial(number):
+    if number == 1:
+        return 1
+    return number * factorial(number - 1)
+
+print(factorial(5))
