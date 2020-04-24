440c5fa5d5d4cc1eb207174f5762a30e6e8df263
Jacques Fidelia Jr <jacques.fidelia@gmail.com>
2020-04-24 23:28:55 +0000

    addition to factorial

--- recursion.py
+++ recursion.py
@@ -18,3 +18,24 @@ def factorial(number):
     return number * factorial(number - 1)
 
 print(factorial(5))
+
+def factorial(number):
+    if number == 1:
+        return 1
+    return number * factorial(number - 1)
+
+print(factorial(5))
+
+def factorial(number):
+    if number == 1:
+        return 1
+    return number * factorial(number - 1)
+
+print(factorial(5))
+
+def factorial(number):
+    if number == 1:
+        return 1
+    return number * factorial(number - 1)
+
+print(factorial(5))
