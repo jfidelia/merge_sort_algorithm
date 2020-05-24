2abd9da900f49f1b30090993388093cb0df02e3b
Jacques Fidelia Jr <jacques.fidelia@gmail.com>
2020-05-24 20:45:25 +0000

    more practice commit

--- merge_sort_practice.py
+++ merge_sort_practice.py
@@ -1,5 +1,43 @@
+# def mergeSort(alist):
+#     print("splitting", alist)
+#     if len(alist) > 1:
+#         mid = len(alist) // 2
+#         left = alist[:mid]
+#         right = alist[mid:]
+
+#         mergeSort(left)
+#         mergeSort(right)
+
+#         i = 0
+#         j = 0
+#         k = 0
+
+#         while i < len(left) and j < len(right):
+#             if left[i] < right [j]:
+#                 alist[k] = left [i]
+#                 i += 1
+#             else:
+#                 alist[k] = right[j]
+#                 j += 1
+#             k += 1
+
+#         while i < len(left):
+#             alist[k] = left[i]
+#             i += 1
+#             k += 1
+
+#         while j < len(right):
+#             alist[k] = right[j]
+#             j += 1
+#             k += 1
+#     print("Merging", alist)
+
+# alist = [22,3,15,17,2,10,19,13]
+# mergeSort(alist)
+# print(alist)
+
 def mergeSort(alist):
-    print("splitting", alist)
+    print("Splitting", alist)
     if len(alist) > 1:
         mid = len(alist) // 2
         left = alist[:mid]
@@ -8,13 +46,11 @@ def mergeSort(alist):
         mergeSort(left)
         mergeSort(right)
 
-        i = 0
-        j = 0
-        k = 0
+        i=j=k=0
 
         while i < len(left) and j < len(right):
-            if left[i] < right [j]:
-                alist[k] = left [i]
+            if left[i] < right[j]:
+                alist[k] = left[i]
                 i += 1
             else:
                 alist[k] = right[j]
@@ -32,8 +68,6 @@ def mergeSort(alist):
             k += 1
     print("Merging", alist)
 
-alist = [22,3,15,17,2,10,19,13]
+alist = [77,22,55,78,90,20,5,2,18,99,200,150]
 mergeSort(alist)
 print(alist)
-
-
