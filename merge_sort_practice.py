67f0b2d16b833b5d4c7b3074fd0cd8499fcde7fe
Jacques Fidelia Jr <jacques.fidelia@gmail.com>
2020-05-27 00:41:33 +0000

    quick sort practice day 3 commit

--- merge_sort_practice.py
+++ merge_sort_practice.py
@@ -36,3 +36,38 @@
 # mergeSort(alist)
 # print(alist)
 
+def mergeSort(alist):
+    print("Splitting", alist)
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        left = alist[:mid]
+        right = alist[mid:]
+
+        mergeSort(left)
+        mergeSort(right)
+
+        i=j=k=0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                alist[k] = left[i]
+                i += 1
+            else:
+                alist[k] = right[j]
+                j += 1
+            k +=1
+
+        while i < len(left):
+            alist[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            alist[k] = right[j]
+            j += 1
+            k += 1
+    print("Merging", alist)
+
+alist = [66,44,77,23,99,1,6,22,100,200,40,50,2]
+mergeSort(alist)
+print(alist)
