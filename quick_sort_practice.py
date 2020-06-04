71b0309014b3e2d1b736776db1bc29bfcedd4f43
Jacques Fidelia Jr <jacques.fidelia@gmail.com>
2020-06-04 20:51:08 +0000

    day 10 merge and quick sort practice commit

--- quick_sort_practice.py
+++ quick_sort_practice.py
@@ -88,3 +88,47 @@
 # quickSort(alist)
 # print(alist)
 
+def quickSort(alist):
+    quickSortHelper(alist, 0, len(alist) - 1)
+
+def quickSortHelper(alist, first, last):
+    if first < last:
+
+        splitpoint = partition(alist, first, last)
+
+        quickSortHelper(alist, first, splitpoint - 1)
+        quickSortHelper(alist, splitpoint + 1, last)
+
+def partition(alist, first, last):
+
+    pivotvalue = alist[first]
+
+    lefthelper = first + 1
+    righthelper = last
+
+    done = False
+    while not done:
+
+        while lefthelper <= righthelper and alist[lefthelper] <= pivotvalue:
+            lefthelper = lefthelper + 1
+
+        while alist[righthelper] >= pivotvalue and righthelper >= lefthelper:
+            righthelper = righthelper - 1
+
+        if righthelper < lefthelper:
+            done = True
+        else:
+            temp = alist[lefthelper]
+            alist[lefthelper] = alist[righthelper]
+            alist[righthelper] = temp
+
+    temp = alist[first]
+    alist[first] = alist[righthelper]
+    alist[righthelper] = temp
+
+    return righthelper
+
+alist = [77,567,88,66,44,33,243,890,10,3,1,4]
+print(alist)
+quickSort(alist)
+print(alist)
