fd4739db6b7eded6ef384abb860ac5aeb441c2d6
Jacques Fidelia Jr <jacques.fidelia@gmail.com>
2020-06-14 23:11:33 +0000

    delete day 15 practice

--- merge_sort_practice.py
+++ merge_sort_practice.py
@@ -36,39 +36,3 @@
 # mergeSort(alist)
 # print(alist)
 
-def mergeSort(alist):
-    print("Splitting ", alist)
-    if len(alist) > 1:
-        mid = len(alist) // 2
-        left = alist[:mid]
-        right = alist[mid:]
-
-        mergeSort(left)
-        mergeSort(right)
-
-        i=j=k=0
-
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-                alist[k] = left[i]
-                i += 1
-            else:
-                alist[k] = right[j]
-                j += 1
-            k += 1
-
-        while i < len(left):
-            alist[k] = left[i]
-            i += 1
-            k += 1
-
-        while j < len(right):
-            alist[k] = right[j]
-            j += 1
-            k += 1
-
-    print("Merging ", alist)
-
-alist = [66,77,88,99,200,44,22,11,1,4,6,8]
-mergeSort(alist)
-print(alist)
