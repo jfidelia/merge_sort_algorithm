3613d27d9fa0e80e0c24ac22d8031552de5a7f74
Jacques Fidelia Jr <jacques.fidelia@gmail.com>
2020-06-17 02:08:22 +0000

    day 17 practice merge and quick sort commit

--- tries.py
+++ tries.py
@@ -114,11 +114,72 @@ from collections import defaultdict
 #         return True
 
 
-# Your Trie object will be instantiated and called as such:
+# # Your Trie object will be instantiated and called as such:
 # obj = Trie()
 # obj.insert("pello")
 # obj.insert("pelloworld")
-# param_2 = obj.search("word")
-# param_3 = obj.startsWith("p")
-# print(param_3)
+# param_2 = obj.search("pello")
+# param_3 = obj.startsWith("pel")
+# print(param_2)
+
+class TrieNode:
+    def __init__(self, char):
+        self.children = []
+        self.isWord = False
+        self.val = char
+
+class Trie(object):
+
+    def __init__(self):
+        self.root = TrieNode("*")
+
+    def insert(self, word):
+        current = self.root
+        for w in word:
+            flag = False
+            for child in current.children:
+                if child.val == w:
+                    flag = True
+                    current = child
+                    break
+            if not flag:
+                new_node = TrieNode(w)
+                current.children.append(new_node)
+                current = new_node
+        current.isWord = True
+
+    def search(self, word):
+        current = self.root
+        for w in word:
+            found = False
+            for child in current.children:
+                if child.val == w:
+                    found = True
+                    current = child
+                    break
+            if not found:
+                return False
+        return current.isWord
+
+    def startsWith(self, prefix):
+        current = self.root
+        for w in prefix:
+            found = False
+            for child in current.children:
+                if child.val == w:
+                    found = True
+                    current = child
+                    break
+            if not found:
+                return False
+        return True
+
+obj = Trie()
+obj.insert("provide")
+obj.insert("provided")
+obj.insert("provides")
+print(obj.search("provided"))
+print(obj.startsWith("provides"))
+param1 = obj.search("provide")
+print(param1)
 
