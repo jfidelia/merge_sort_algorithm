3c1da53eae6211049623a78462e876f86258fcc0
Jacques Fidelia Jr <jacques.fidelia@gmail.com>
2020-07-16 23:17:10 +0000

    day 33 practice

--- tries.py
+++ tries.py
@@ -59,3 +59,59 @@ from collections import defaultdict
 # obj.insert(hello)
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
+class TrieNode:
+
+    def __init__(self, char):
+        self.children = []
+        self.isWord = False
+        self.val = char
+
+class Trie(object):
+    def __init__(self):
+        self.root = TrieNode('*')
+
+    def insert(self, word):
+        current = self.root
+        for w in word:
+            flag = False
+            for child in current.children:
+                if child.val == w:
+                    flag = True
+                    current = child
+                    break
+            if not flag:
+                new_node = TrieNode(w)
+                current.children.append(new_node)
+                current = new_node
+        current.isWord = True
+
+    def search(self, word):
+        current = self.root
+        for w in word:
+            found = False
+            for child in current.children:
+                if child.val == w:
+                    found = True
+                    current = child
+                    break
+            if not found:
+                return False
+        return current.isWord
+
+    def startsWith(self, prefix):
+        current = self.root
+        for w in prefix:
+            found = False
+            for child in current.children:
+                if child.val == w:
+                    found = True
+                    current = child
+                    break
+            if not found:
+                return False
+        return True
+
+obj = Trie()
+obj.insert('word')
+print(obj.search('words'))
+print(obj.startsWith('words'))
